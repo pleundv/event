3a80990bf386bde6fe959c1a5fa35746b754bbe0
pleundv <84378584+pleundv@users.noreply.github.com>
2021-07-02 16:48:38 +0200

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,23 @@
+
+
+const attachEventToAlertButton = function () {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function () {
+        alert("Hello World!");
+    });
+};
+
+const toggleColor = function () {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle("red-background");
+};
+
+const attachEventToChangeColorButton = function () {
+    const changeColorButton = document.getElementById("change-background-button");
+    changeColorButton.addEventListener("click", function () {
+        toggleColor();
+    });
+};
+
+attachEventToAlertButton();
+attachEventToChangeColorButton();
